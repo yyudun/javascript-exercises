3042d593ee15e82d9d0e7efd0611626eea1f4522
Y. Udun <113389799+yyudun@users.noreply.github.com>
2023-02-14 18:29:44 +0300

    first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if(num > 0 || num === 0){
+        for (let i = 0; i < num; i++){
+            result += string;
+        }
+    } else {
+        result = 'ERROR';
+    }
+   
+    return result;
 };
 
 // Do not edit below this line
