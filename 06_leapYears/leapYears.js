d91bf85d11434d3ca4063130c4cc9dc94cead12a
Y. Udun <113389799+yyudun@users.noreply.github.com>
2023-02-28 00:14:25 +0300

    js exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    let result;
+    if(num % 400 === 0){
+        result = true;
+    } else if(num % 100 === 0){
+        result = false;
+    } else if(num % 4 === 0){
+        result = true;
+    } else {
+        result = false
+    }
+    return result
 
 };
 
